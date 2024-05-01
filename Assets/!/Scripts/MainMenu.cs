22139bea3a448f08532cd29142714296fc830142
SHussain <husain187@hotmail.com>
2024-05-02 00:17:31 +0300

    main menu 80% done

---
+++ Assets/!/Scripts/MainMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    private Canvas mainMenu;
+    private Canvas credits;
+    private Canvas instructions;
+
+    private void Start()
+    {
+        //find all the canvases
+        mainMenu = GameObject.Find("CanvasMenu").GetComponent<Canvas>();
+        credits = GameObject.Find("CanvasCredits").GetComponent<Canvas>();
+        instructions = GameObject.Find("CanvasInstructions").GetComponent<Canvas>();
+        mainMenu.enabled = true;
+    }
+
+    public void startGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void loadInstructions()
+    {
+        mainMenu.enabled = false;
+        credits.enabled = false;
+
+        instructions.enabled = true;
+    }
+
+    public void loadCredits()
+    {
+        mainMenu.enabled = false;
+        instructions.enabled = false;
+
+        credits.enabled = true;
+    }
+
+    public void backToMenu()
+    {
+        credits.enabled = false;
+        instructions.enabled = false;
+
+        mainMenu.enabled = true;
+    }
+
+    public void quitGame ()
+    {
+        Application.Quit();
+    }
+}
