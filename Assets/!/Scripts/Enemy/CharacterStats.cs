3eef60797bf0c1606375bf90de09d9dc55943d89
Short Man <yousif746@gmail.com>
2024-05-04 23:51:25 +0300

    Added zombie tracker AI and attack animation

---
+++ Assets/!/Scripts/Enemy/CharacterStats.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats: MonoBehaviour
+{
+
+    [SerializeField] protected int health;
+    [SerializeField] protected int maxHealth;
+
+    [SerializeField] protected bool isDead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitVariables();
+    }
+
+    public virtual void CheckHealth() {
+
+        if (health <= 0) {
+        
+            health = 0;
+            Die();
+        }
+        if (health >= maxHealth)
+        {
+        
+            health = maxHealth;
+        
+        }
+    }
+
+    public void SetHealthTo(int healthToSetTo)
+    {
+    
+        health = healthToSetTo;
+        CheckHealth();
+    
+    }   
+
+    public void TakeDamage(int damage)
+    {
+    
+        int healthAfterDamage =  health - damage;
+        SetHealthTo(healthAfterDamage); 
+    
+    }   
+
+    public void Heal(int healAmount)
+    {
+    
+        int healthAfterHeal = health + healAmount;
+        SetHealthTo(healthAfterHeal);   
+    
+    }
+    public virtual void InitVariables()
+    {
+    
+        maxHealth = 100;
+        SetHealthTo(maxHealth);
+        isDead = false;
+    
+    }
+
+    public virtual void Die() { 
+    
+    isDead = true;
+
+    }
+
+}
---
+++ Assets/!/Scripts/Enemy/ZombieStats.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieStats : CharacterStats
+{
+    [SerializeField] private int damage;
+    [SerializeField] private float attackspeed;
+
+    [SerializeField] private bool canAttack;
+
+
+    private void Start()
+    {
+        InitVariables();
+    }
+
+
+    public void DealDamage(CharacterStats statsToDamage)
+    {
+        statsToDamage.TakeDamage(damage);
+    } 
+    
+    public override void Die()
+    {
+    
+        base.Die();
+        Destroy(gameObject);
+
+    }
+
+    public override void InitVariables()
+    {
+
+        maxHealth = 25;
+        SetHealthTo(maxHealth);
+        damage = 10;
+        attackspeed = 1.5f;
+        canAttack = true;
+        isDead = false;
+
+    }
+
+
+}
